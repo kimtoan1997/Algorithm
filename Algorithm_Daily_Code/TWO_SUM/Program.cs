a220ea0d56a5d8aa45525d333576c180d1d795ac
Toan <kimtoan1997.ld@gmail.com>
2023-01-20 20:57:56 +0700

    Add code result Two Sum number

---
+++ Algorithm_Daily_Code/TWO_SUM/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace TWO_SUM
+{
+    internal class Program
+    {
+        static void Main (string[] args)
+        {
+            int[] array = { 3, 2, 4 };
+
+            var result = Two_Sum(array, 6);
+
+            foreach (int i in result)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadLine();
+        }
+
+        static int[] Two_Sum(int[] array, int target) 
+        {
+            int[] result = new int[2];
+
+            
+            for (int i = 0; i < array.Length; i++)
+            {
+                int firstArrayElement = array[i];
+                int remainElement = target - firstArrayElement;
+                int matchIndex = array.ToList().IndexOf(remainElement);
+
+                if (matchIndex != i && matchIndex != -1)
+                {
+                    result[0] = i;
+                    result[1] = matchIndex;
+
+                    return result;
+                }
+            }
+            return result;
+        }
+    }
+}
